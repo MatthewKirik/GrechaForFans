d9ccb93d69fd7403a68a7bee4ce4e58df55e0135
Матвей Кирик <ikirik59@gmail.com>
2021-01-21 19:24:23 +0200

    Add buisness logic services

---
+++ server/GrechaForFans/BLL/Services/ILotsService.cs
@@ -0,0 +1,19 @@
+﻿using DataTransfer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public interface ILotsService
+    {
+        Task<LotDto> GetLot(int lotId);
+        Task<List<LotDto>> GetLots();
+        Task<List<LotDto>> GetLots(int shopId);
+        Task<List<LotDto>> GetCheapestLots(int amount);
+        Task<List<LotDto>> GetCheapestLots(int shopId, int amount);
+        Task<List<LotDto>> GetCheapestLots(int amount, DateTime afterDate);
+        Task<List<LotDto>> GetCheapestLots(int shopId, int amount, DateTime afterDate);
+    }
+}
---
+++ server/GrechaForFans/BLL/Services/IShopsService.cs
@@ -0,0 +1,14 @@
+﻿using DataTransfer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public interface IShopsService
+    {
+        Task<List<ShopDto>> GetShops();
+        Task<ShopDto> GetShop(int shopId);
+    }
+}
