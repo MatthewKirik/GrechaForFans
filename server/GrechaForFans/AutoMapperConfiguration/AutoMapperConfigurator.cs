05b18c196d01d8c99215491584ffcaff3a55be49
Матвей Кирик <ikirik59@gmail.com>
2021-01-21 17:43:41 +0200

    Add automapper configuration

---
+++ server/GrechaForFans/AutoMapperConfiguration/AutoMapperConfigurator.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMapperConfiguration
+{
+    public static class AutoMapperConfigurator
+    {
+        public static IMapper GetMapper()
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            return mapper;
+        }
+    }
+}
---
+++ server/GrechaForFans/AutoMapperConfiguration/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMapperConfiguration
+{
+    internal class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+        }
+    }
+}
