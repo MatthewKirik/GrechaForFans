563ef5747b5f24565d8be5a6891c33dcb2a8d5fc
Матвей Кирик <ikirik59@gmail.com>
2021-01-21 18:07:35 +0200

    Add dtos-to-entities mappings

--- server/GrechaForFans/AutoMapperConfiguration/MappingProfile.cs
+++ server/GrechaForFans/AutoMapperConfiguration/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using DAL.Models;
+using DataTransfer;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +11,9 @@ namespace AutoMapperConfiguration
     {
         public MappingProfile()
         {
+            CreateMap<Lot, LotDto>().ReverseMap();
+            CreateMap<Price, PriceDto>().ReverseMap();
+            CreateMap<Shop, ShopDto>().ReverseMap();
         }
     }
 }
