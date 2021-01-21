fe012d5cfd6fec1d0611ccad73a798db953d487f
Матвей Кирик <ikirik59@gmail.com>
2021-01-21 16:34:53 +0200

    Add PricesController

---
+++ server/GrechaForFans/API/Controllers/PricesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricesController : ControllerBase
+    {
+        [HttpGet("lowest")]
+        public async Task<IEnumerable<string>> GetLowestPrices()
+        {
+            return new string[] { "hi", "there" };
+        }
+    }
+}
--- server/GrechaForFans/API/Startup.cs
+++ server/GrechaForFans/API/Startup.cs
@@ -16,6 +16,7 @@ namespace API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,7 @@ namespace API
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+                endpoints.MapControllers();
             });
         }
     }
