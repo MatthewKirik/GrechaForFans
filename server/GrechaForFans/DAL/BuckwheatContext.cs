92f97f8a268f4c965d3f4321baeaeceb142a5d30
Матвей Кирик <ikirik59@gmail.com>
2021-01-21 16:56:04 +0200

    Add database context

---
+++ server/GrechaForFans/DAL/BuckwheatContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    internal class BuckwheatContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=buckwheat.db");
+    }
+}
