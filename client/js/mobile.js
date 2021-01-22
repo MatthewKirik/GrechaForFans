43aa33ed448c736b42e7905e243b324a912159f6
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-01-23 00:55:30 +0200

    Designed menu for mobile ver.

---
+++ client/js/mobile.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector("#menu-block");
+const arrow_box = document.querySelector("#arrow-box");
+
+const showMenu = () => {
+	menu.style = "left: 0%";
+	arrow_box.style = "left: 50%; transform: rotateY(180deg);";
+	menu.hidden = false;
+}
+const hideMenu = () => {
+	menu.style = "left: -75%";
+	arrow_box.style = "left: 0; transform: rotateY(0deg);";
+	menu.hidden = true;
+}
+
+arrow_box.onclick = () => menu.hidden ? showMenu() : hideMenu();
