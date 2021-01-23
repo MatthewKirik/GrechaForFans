cb3c3083568eb33ca71606f62e2445e87af53ff2
Матвей Кирик <ikirik59@gmail.com>
2021-01-23 13:33:29 +0200

    Add prom ua stub implementation

---
+++ server/GrechaForFans/BLL/Parsers/IParser.cs
@@ -0,0 +1,13 @@
+﻿using DataTransfer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Parsers
+{
+    interface IParser
+    {
+        public Task<List<LotDto>> ParseLots(int pagesAmount);
+    }
+}
---
+++ server/GrechaForFans/BLL/Parsers/Implementations/PromUaParser.cs
@@ -0,0 +1,23 @@
+﻿using DataTransfer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Parsers.Implementations
+{
+    public class PromUaParser : IParser
+    {
+        AngleSharp.IBrowsingContext browsingContext;
+        public PromUaParser()
+        {
+            var config = AngleSharp.Configuration.Default;
+            this.browsingContext = AngleSharp.BrowsingContext.New(config);
+        }
+
+        public Task<List<LotDto>> ParseLots(int pagesAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
