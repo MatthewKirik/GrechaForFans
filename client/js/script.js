8ccd0105c0a4ed1db0c97c6bc33265b8abeee03b
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-01-24 20:25:30 +0200

    Displaying feature done for JSON data

--- client/js/script.js
+++ client/js/script.js
@@ -16,34 +16,52 @@ document.addEventListener("DOMContentLoaded", () => {
 	};
 
 	const displayLots = data => {
-		const shopOffers = document.querySelectorAll(".offer-block");
+		const shopOffers = document.querySelectorAll(".offer");
 
-		for (let i = 0; i < shopOffers.length; i++) {
-			// shopOffers[i].innerHTML = "";
+		shopOffers.forEach(el => {
+			el.remove();
+		});
 
-			for (let j = 0; j < data.length; j++) {
-				const currentLot = data[j];
-				const offer = document.createElement("div");
-				const offerImage = document.createElement("img");
+		for (let i = 0; i < data.length; i++) {
+			const currentLot = data[i];
+			const shopName = currentLot.shop;
 
-				offer.classList.add("offer");
-				offerImage.src = currentLot.imageLink;
+			const shopElement = document.querySelector(`.offer-block[id="${shopName}-offers"]`);
+			const offer = document.createElement("div");
+			const offerImage = document.createElement("img");
+			const textArea = document.createElement("div");
+			const title = document.createElement("h4");
+			const price = document.createElement("p");
+			const link = document.createElement("a");
 
-				shopOffers[i].appendChild(offer);
-				offer.appendChild(offerImage);
-			}
+			offer.classList.add("offer");
+			textArea.classList.add("text-area");
+			title.id = "title";
+			price.id = "price";
+
+			Object.assign(offer, data);
+			console.log(offer);
+			offerImage.src = currentLot.imageLink;
+			title.innerText = currentLot.title;
+			price.innerText = currentLot.price + ` грн/${currentLot.weightInGrams}г`;
+			link.href = currentLot.link;
+			link.innerText = "Перейти на сайт";
+
+			shopElement.append(offer);
+			offer.append(offerImage,textArea);
+			textArea.append(title, price, link);
 		}
 	}
 
-	// displayLots([
-	// 	{
-	// 		"title": "Êðóïà ãðå÷íåâàÿ, 212 ã",
-	// 		"imageLink": "https://images.ua.prom.st/2683272104_w200_h200_krupa-grechnevaya-212.jpg",
-	// 		"link": "https://prom.ua/p1280075973-krupa-grechnevaya-212.html",
-	// 		"manufacturer": null,
-	// 		"weightInGrams": 212,
-	// 		"shop": null,
-	// 		"price": null,
-	// 		"id": 1
-	// 																								}]);
+	displayLots([
+		{
+			"title": "Êðóïà ãðå÷íåâàÿ, 212 ã",
+			"imageLink": "https://images.ua.prom.st/2683272104_w200_h200_krupa-grechnevaya-212.jpg",
+			"link": "https://prom.ua/p1280075973-krupa-grechnevaya-212.html",
+			"manufacturer": null,
+			"weightInGrams": 212,
+			"shop": "prom",
+			"price": null,
+			"id": 1
+																									}]);
 });
